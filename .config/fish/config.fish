aac5197cafe3922d536afaeca3e9e86a13ab1705
Vladyslav Dukhin <15850680+primeare@users.noreply.github.com>
2026-05-10 01:31:19 +0200

    Update VS Code settings and Fish config
    Signed-off-by: Vladyslav Dukhin <15850680+primeare@users.noreply.github.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,19 +1,29 @@
 # add local executables to PATH
 fish_add_path "$HOME/.npm-packages/bin"
 fish_add_path "$HOME/.bin"
+fish_add_path "$HOME/.local/bin"
 
 # add Visual Studio Code (code) to PATH
-fish_add_path "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+if test -d "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+  fish_add_path "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+end
 
 # command aliases and shortcuts
 alias c="clear"
 
 # set Visual Studio Code as a default text editor
-set --global --export EDITOR code --wait --new-window
-set --global --export VISUAL code --wait --new-window
+if command -q code
+  set --global --export EDITOR "code --wait --new-window"
+  set --global --export VISUAL "code --wait --new-window"
+else
+  set --global --export EDITOR nano
+  set --global --export VISUAL nano
+end
 
-# initialise Starship
-starship init fish | source
+if status is-interactive
+  # initialise Starship
+  starship init fish | source
 
-# enable transient prompt
-enable_transience
+  # enable transient prompt
+  enable_transience
+end
